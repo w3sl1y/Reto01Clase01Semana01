da0f2f3e34155b8de470f021c134ed3b958923e2
w3sl1y <SV73623892@idat.edu.pe>
2021-08-29 19:13:53 -0500

    Actualizado

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert(HOLA)
+window.onload = iniciar;
+
+function iniciar() {
+ var btnCalcular = document.getElementById("btnCalcular");
+ btnCalcular.addEventListener("click", clickBtnCalcular);
+}
+
+function clickBtnCalcular() {
+    var txtPeso = document.getElementById("txtPeso");
+    var peso = txtPeso.value;
+
+    var txtAltura = document.getElementById("txtAltura");
+    var altura = txtAltura.value;
+
+    var imc = peso / (altura * altura);
+    alert("Su IMC es de " + imc)
+}
